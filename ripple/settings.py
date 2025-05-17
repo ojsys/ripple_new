90c61db49bd9eec827a1550cd7b3ea23cb11448b
Onah Jonah <onahjonah@gmail.com>
2025-05-17 12:02:29 +0100

    updated the settings for templates

--- ripple/settings.py
+++ ripple/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'ripple.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'projects', 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
