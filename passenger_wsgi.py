fbdcfa15fb644148c9cc2375a073d365b6233f56
Jonah Onah <onahjonah@gmail.com>
2026-01-18 20:44:20 +0100

    Updated passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,12 +2,12 @@ import os
 import sys
 
 # Add your project directory to the sys.path
-path = '/home/username/ripple'
+path = '/home/startupr/ripple'
 if path not in sys.path:
     sys.path.insert(0, path)
 
 # Set environment variable to use production settings
-os.environ['DJANGO_SETTINGS_MODULE'] = 'ripple.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ripple.settings.production'
 
 # Create application object
 from django.core.wsgi import get_wsgi_application
