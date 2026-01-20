16dbba152660a2f5466f62157683296b42a4c349
Jonah Onah <onahjonah@gmail.com>
2026-01-20 08:21:04 +0100

    updated site settings

--- apps/cms/context_processors.py
+++ apps/cms/context_processors.py
@@ -1,2 +1,12 @@
+from .models import SiteSettings
+
+
 def site_settings(request):
-    return {}
+    """
+    Context processor to make site settings available in all templates.
+    """
+    try:
+        settings = SiteSettings.load()
+        return {'site_settings': settings}
+    except Exception:
+        return {'site_settings': None}
