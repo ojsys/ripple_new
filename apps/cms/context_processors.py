cc6949ba464dfceea05a7804c1af6bbae57e331f
Jonah Onah <onahjonah@gmail.com>
2026-01-23 14:55:06 +0100

    Fixed Type error

--- apps/cms/context_processors.py
+++ apps/cms/context_processors.py
@@ -1,6 +1,15 @@
-from .models import SocialMediaLink
+from .models import SocialMediaLink, SiteSettings
 
 def social_links(request):
     return {
         'social_links': SocialMediaLink.objects.all().order_by('order')
+    }
+
+def site_settings(request):
+    try:
+        settings = SiteSettings.load()
+    except Exception:
+        settings = None
+    return {
+        'site_settings': settings
     }
