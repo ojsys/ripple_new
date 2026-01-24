779427aeb96263528acf1d1590ab4686a9fddb0e
Jonah Onah <onahjonah@gmail.com>
2026-01-24 20:14:35 +0100

    Updated the sidesign for investment page admin error

--- apps/cms/admin.py
+++ apps/cms/admin.py
@@ -227,5 +227,5 @@ class LegalPageAdmin(admin.ModelAdmin):
     def get_readonly_fields(self, request, obj=None):
         """Make page_type readonly after creation to prevent duplicates."""
         if obj:
-            return list(self.readonly_fields) + ['page_type']
-        return self.readonly_fields
+            return ['created_at', 'updated_at', 'page_type']
+        return ['created_at', 'updated_at']
