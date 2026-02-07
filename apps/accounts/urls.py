0160693bcaff5e802b611fd7ab418477aba3f96d
Jonah Onah <onahjonah@gmail.com>
2026-02-07 21:15:55 +0100

    updated password reset system III

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -11,13 +11,14 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
 
     # Password reset URLs with custom templates
-    path('password_reset/', 
+    path('password_reset/',
          auth_views.PasswordResetView.as_view(
              template_name='registration/password_reset_form.html',
-             email_template_name='registration/password_reset_email.html',
+             email_template_name='registration/password_reset_email.txt',
+             html_email_template_name='registration/password_reset_email.html',
              subject_template_name='registration/password_reset_subject.txt',
              success_url='/accounts/password_reset/done/',
-         ), 
+         ),
          name='password_reset'),
     
     path('password_reset/done/', 
